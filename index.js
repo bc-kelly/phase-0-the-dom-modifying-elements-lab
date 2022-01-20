c32a7acc2e5f7359f898126bb88cae7ec1c9e8e3
Bridget Kelly <bckelly197@gmail.com>
2022-01-19 23:43:10 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.querySelector('main#main').remove();
+
+const newHeader = document.createElement ('H1');
+newHeader.id = ('victory');
+newHeader.innerHTML = "<h1>Bridget is the champion</h1>";
